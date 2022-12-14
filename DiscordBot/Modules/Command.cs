3438239d4e948a489fad1a9cd938c82653127fc9
Sinforge <vlad.vlasov77@mail.ru>
2022-12-14 12:15:52 +0300

    fix bug with recognizing commands

---
+++ DiscordBot/Modules/Command.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules
+{
+    public class Command : ModuleBase<SocketCommandContext>
+    {
+
+        [Command("who-noob")]
+        public async Task ping()
+        {
+            await ReplyAsync("HeqlinXNX");
+        }
+    }
+}
--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,35 +1,84 @@
-﻿using Discord;
+﻿using System.Reflection;
+using Discord;
 using Discord.Commands;
+using Discord.Net.Providers.WS4Net;
 using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
 
 namespace DiscordBot
 {
     public class Program
     {
-        private DiscordSocketClient _client;
-        private CommandService _commands;
-        public static Task Main(string[] args) => new Program().MainAsync();
+        DiscordSocketClient client;
+            CommandService command;
+            IServiceProvider service;
+            static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
 
-        public async Task MainAsync()
-        {
-            _client = new DiscordSocketClient();
-            _commands = new CommandService();
-            _client.Log += Log;
-            var token = "MTA1MjI0NDk5NTU4OTAyNTkyNQ.GlwkyP.gCC8yc30ZKI994P4IpEz4jnmxbTKOro0dX-EQE";
-            await _client.LoginAsync(TokenType.Bot, token);
-            await _client.StartAsync();
-            //Hook into the message received event, this is how we handle the hello world example
-            await Task.Delay(-1);
-        }
+            public async Task RunBotAsync()
+            {
+                client = new DiscordSocketClient(new DiscordSocketConfig
+                {
+                    WebSocketProvider = WS4NetProvider.Instance,
+                    GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+                });
+                command = new CommandService();
+                //Add Singletons
+                service = new ServiceCollection()
+                    .AddSingleton(client)
+                    .AddSingleton(command)
+                    .BuildServiceProvider();
 
-        private Task Log(LogMessage msg)
-        {
-            Console.WriteLine(msg.ToString());
-            return Task.CompletedTask;
-        }
-        private async Task CommandExecuted(Optional<CommandInfo> command, ICommandContext context, IResult result)
-        {
-            // a command execution was attempted
-        }
+
+                //Add logs
+                client.Log += Log;
+
+                //Add command handler
+                client.MessageReceived += MessageReceived;
+
+
+                //Connect Command.cs file
+                await command.AddModulesAsync(Assembly.GetEntryAssembly(), service);
+
+                string token;
+
+
+                string workingDirectory = Environment.CurrentDirectory;
+                string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+                using (TextReader reader = File.OpenText(projectDirectory + "\\config.json"))
+                {
+                    JObject json = JObject.Parse(reader.ReadToEnd());
+                    Console.WriteLine(json.ToString());
+                    Console.WriteLine(json["Token"].ToString());
+                    token = json["Token"].ToString();
+
+                }
+                await client.LoginAsync(TokenType.Bot, token);
+                await client.StartAsync();
+                await Task.Delay(-1);
+            }
+
+
+            //Method for handling commands
+            private async Task MessageReceived(SocketMessage message)
+            {
+                var _message = message as SocketUserMessage;
+                if (_message is null || message.Author.IsBot) return;
+                int argPos = 0;
+                if (_message.HasStringPrefix("$$", ref argPos) || _message.HasMentionPrefix(client.CurrentUser, ref argPos))
+                {
+                    var txt = new SocketCommandContext(client, _message);
+                    var result = await command.ExecuteAsync(txt, argPos, service);
+                    if (!result.IsSuccess)
+                        Console.WriteLine(result.ErrorReason);
+                }
+            }
+
+            private Task Log(LogMessage msg)
+            {
+                Console.WriteLine(msg.ToString());
+
+                return Task.CompletedTask;
+            }
     }
 }
