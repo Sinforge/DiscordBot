b3efd654a6e80bb385191001d86d2ef78935e57c
Sinforge <vlad.vlasov77@mail.ru>
2023-02-02 18:31:00 +0300

    connect selenium

--- DiscordBot/Services/DotaStatsService.cs
+++ DiscordBot/Services/DotaStatsService.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
 
 namespace DiscordBot.Services
 {
-    internal class DotaStatsService
+    public class DotaStatsService
     {
+        public async void GetMostPlayedHeroes(ulong dotabuffId)
+        {
+            IWebDriver driver = new EdgeDriver();
+            INavigation nav = driver.Navigate();
+            nav.GoToUrl($"https://www.dotabuff.com/players/{dotabuffId}/heroes");
+
+            var element = driver.FindElement(By.TagName("table"));
+            Console.WriteLine(element.Text);
+        }
+    }
+
+    public class MostPlayedHeroesEntity
+    {
+        public string Name { get; set; }
+        public int Matches { get; set; }
+
+        public float WinRate { get; set; }
+        public float KdaRatio  { get; set; }
     }
 }
